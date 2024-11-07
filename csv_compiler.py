95c2c885a5a126f77cd2365c3adfc43262500801
Ali <114579912+BigBootyAndreas@users.noreply.github.com>
2024-11-07 13:50:42 +0100

    run1

--- csv_compiler.py
+++ csv_compiler.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 
 # Specify the folder containing the compressed .npz files
-folder_path = 'C:\\Users\\Ali\\Downloads\\data7'
+folder_path = 'C:\\Users\\Ali\\OneDrive - Aalborg Universitet\\Desktop\\P7\\Data\\data1'
 
 # List all files in the folder
 files = os.listdir(folder_path)
