b315041be1f3ace5ec56ef0fffd27724cf3be5bf
sPedersennn <sfspedersen33@gmail.com>
2024-11-11 09:17:30 +0100

    compiler rm integration

--- csv_compiler.py
+++ csv_compiler.py
@@ -2,32 +2,37 @@ import os
 import numpy as np
 import time
 
-# Specify the folder containing the compressed .npz files
-folder_path = 'C:\\Users\\Ali\\OneDrive - Aalborg Universitet\\Desktop\\P7\\Data\\data1'
+def csv_compiler(folder_path):
 
-# List all files in the folder
-files = os.listdir(folder_path)
+    # List all files in the folder
+    files = os.listdir(folder_path)
 
-# Filter out only the .npz files
-npz_files = [file for file in files if file.endswith(".npz")]
-# Convert the .npz files to .csv files saving the timestamp in the first column,
+    # Filter out only the .npz files
+    npz_files = [file for file in files if file.endswith(".npz")]
 
-# the time in the secound column, and the amplitude in the third
-for filename in npz_files:
-    full_path = os.path.join(folder_path, filename)
-    loaded = np.load(full_path)
-    data = loaded["a"]
-    csv_filename = filename.replace(".npz", ".csv")
-    csv_filepath = os.path.join(folder_path, csv_filename)
-    np.savetxt(csv_filepath, data, delimiter=",", fmt="%s", header="Timestamp, Time (s), Amplitude", comments="")
-    print(f"Converted {filename}")
+    # the time in the secound column, and the amplitude in the third
+    for filename in npz_files:
+        full_path = os.path.join(folder_path, filename)
+        loaded = np.load(full_path)
+        data = loaded["a"]
+        csv_filename = filename.replace(".npz", ".csv")
+        csv_filepath = os.path.join(folder_path, csv_filename)
+        np.savetxt(csv_filepath, data, delimiter=",", fmt="%s", header="Timestamp, Time (s), Amplitude", comments="")
+        print(f"Converted {filename}")
 
-time.sleep(1)
+def npz_rm(folder_path):
 
-for filename in npz_files:
-    full_path = os.path.join(folder_path, filename)
-    
-    # Remove the .npz file
-    os.remove(full_path)
-    print(f"Deleted {filename}")
-    time.sleep(0.2)
+    files = os.listdir(folder_path)
+
+    # Filter out only the .npz files
+    npz_files = [file for file in files if file.endswith(".npz")]
+
+    # Convert the .npz files to .csv files, saving the timestamp in the first column,
+    # the time in the second column, and the amplitude in the third
+    for filename in npz_files:
+        full_path = os.path.join(folder_path, filename)
+        
+        # Remove the .npz file
+        os.remove(full_path)
+        print(f"Deleted {filename}")
+        time.sleep(0.1)
