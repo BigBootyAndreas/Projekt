d42906c8196e9759e7adf4f52c7c1a48da551bab
sPedersennn <sfspedersen33@gmail.com>
2024-11-12 15:45:20 +0100

    Looping in aucoustic data analysis + fixes

--- Interface.py
+++ Interface.py
@@ -10,6 +10,9 @@ from segmentation_loop import segmentation_analysis
 ###################################################
 
 
+#Interface inputs
+yes = {'yes', 'y', 'ye', ''}
+no = {'no', 'n'}
 
 # Segmentation:
 sample_rate = 16000         # Data sample rate (Hz)
@@ -30,10 +33,6 @@ person_paths = {
     'Theis': 'C:\\Users\\Person4\\Downloads\\data\\',
     'Zahid': 'C:\\Users\\Person5\\Downloads\\data\\',
 }
-
-#Interface inputs
-yes = {'yes', 'y', 'ye', ''}
-no = {'no', 'n'}
 
 def IMU(directory):
     print("Welcome to the IMU analysis tool.")
@@ -64,16 +63,16 @@ def IMU(directory):
             if 0 <= choice < len(imu_files):
                 if choice in file_choice:
                     print("File already seleceted")
-                    if str(input("Are you done? (y/n)")).strip().upper() in yes:
+                    if str(input("Are you done? (y/n)")).strip().upper() == "yes":
                         break
                 else: 
                     file_choice.append(choice)
-                    if str(input("Are you done?(y/n)")).strip().upper() in yes:
+                    if str(input("Are you done? (y/n)")).strip().upper() == "yes":
                         break
         elif k == 0:
             if 0 <= choice < len(imu_files):
                 file_choice.append(choice)
-                if str(input("Are you done? (y/n)")).strip().upper() in yes:
+                if str(input("Are you done? (y/n)")).strip().upper() == "yes":
                     break
                 k += 1
         else:
@@ -96,53 +95,65 @@ def IMU(directory):
 
 
 def acoustic(directory):
+
     print("Welcome to the Acoustic analysis tool.")
     
+    run = True
+
     # List subdirectories in the Acoustic directory
     subdirs = [d for d in os.listdir(directory) if os.path.isdir(os.path.join(directory, d))]
 
     if not subdirs:
         print("No subdirectories found in the Acoustic directory.")
         return
+    
+    while True:
+        
+        print("Available subfolders for Acoustic analysis:")
+        for idx, subdir in enumerate(subdirs):
+            print(f"{idx + 1}. {subdir}")
 
-    print("Available subfolders for Acoustic analysis:")
-    for idx, subdir in enumerate(subdirs):
-        print(f"{idx + 1}. {subdir}")
-
-    sub_choice = int(input("Enter the number corresponding to the subfolder: ")) - 1
-
-    if 0 <= sub_choice < len(subdirs):
-        selected_subdir = subdirs[sub_choice]
-        subdir_path = os.path.join(directory, selected_subdir)
-        print(f"You selected {selected_subdir}. Full path: {subdir_path}")
-
-        convert = input("Would you like to convert the files? (y/n)").lower()
-        if convert in yes:
-            csv_compiler(subdir_path)
-            npz_rm(subdir_path)
-            print("Files converted to CSV and npz files deleted.")
-        elif convert in no:
-            print("Files already CSV.")
-        else:
-            print("Please respond with 'yes' or 'no'.")
-
-        print("Choose which analysis you want to conduct")
-        analysis_choice = input("Enter '1' for FFT analysis or '2' for segment analysis: ")
-
-        if analysis_choice == '1':  # FFT
-
-            # Perform the FFT analysis on the selected subdirectory
-            perform_fft_analysis(subdir_path)
-
-        elif analysis_choice == '2':  # Segmentation
+        sub_choice = int(input("Enter the number corresponding to the subfolder: ")) - 1
             
-            # Perform segmentation analysis
-            segmentation_analysis(subdir_path,window_samples, step_size, sample_rate, sample_dir, output_dir, amp_tol, amp_var_tol)
+        if 0 <= sub_choice < len(subdirs):
+            selected_subdir = subdirs[sub_choice]
+            subdir_path = os.path.join(directory, selected_subdir)
+            print(f"You selected {selected_subdir}. Full path: {subdir_path}")
+            
+            convert = input("Would you like to convert the files? (y/n)").lower()
+            if convert in yes:
+                csv_compiler(subdir_path)
+                npz_rm(subdir_path)
+                print("Files converted to CSV and npz files deleted.")
+            elif convert in no:
+                print("Files already CSV.")
+            else:
+                print("Please respond with 'yes' or 'no'.")
+
+            print("Choose which analysis you want to conduct")
+            analysis_choice = input("1. FFT analysis\n2. Segment analysis\n")
+
+            if analysis_choice == '1':  # FFT
+
+                # Perform the FFT analysis on the selected subdirectory
+                perform_fft_analysis(subdir_path)
+                break
+
+            elif analysis_choice == '2':  # Segmentation
+                    
+                # Perform segmentation analysis
+                segmentation_analysis(subdir_path,window_samples, step_size, sample_rate, sample_dir, output_dir, amp_tol, amp_var_tol)
+
+            else:
+                print("Invalid selection. Please restart and choose '1' or '2'.")
 
         else:
-            print("Invalid selection. Please restart and choose '1' or '2'.")
-    else:
-        print("Invalid selection. Please restart and choose a valid subfolder.")
+            print("Invalid selection. Please restart and choose a valid subfolder.")
+        
+        run_again = input("Run again? (y/n): ").strip().lower()
+        if run_again not in yes:
+            print("Exiting the program.")
+            break
 
 def main():
     print("Welcome to data processing")
