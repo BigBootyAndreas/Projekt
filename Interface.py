b50cc37f1e2c97e0c16c02af7cd4c07a76a0f96a
sPedersennn <sfspedersen33@gmail.com>
2024-11-18 09:59:47 +0100

    Run again fixed

--- Interface.py
+++ Interface.py
@@ -138,17 +138,17 @@ def acoustic(directory):
                 print("Please respond with 'yes' or 'no'.")
 
             print("Choose which analysis you want to conduct")
-            analysis_choice = input("1. FFT analysis\n2. Raw data analysis\n3. Segmentation analysis\n")
+            analysis_choice = input("1. FFT analysis\n2. Raw data analysis\n3. Segmentation analysis\nInput:")
 
             if analysis_choice == '1':  # FFT analysis
                 perform_fft_analysis(subdir_path)
-                break
+                
             elif analysis_choice == '2':  # Raw data analysis
                 plot_raw_data(subdir_path)
-                break
+                
             elif analysis_choice == '3':  # Raw data analysis
                 segmentation_analysis(subdir_path,window_samples, step_size, sample_rate, sample_dir, output_dir, amp_tol, amp_var_tol)
-                break
+                
             else:
                 print("Invalid selection. Please choose '1' or '2'.")
 
